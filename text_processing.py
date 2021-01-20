3b3c47b13a17f6833eb95ad7a15ca085fa8f4142
soooupp <k1bae2301@gmail.com>
2021-01-20 15:40:32 +0900

    second submit code

--- text_processing.py
+++ text_processing.py
@@ -33,7 +33,9 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
+    tmpstr = input_string[:]
+    trimstr = tmpstr.lower().strip()
+    normalized_string = " ".join(trimstr.split())
     return normalized_string
 
 
@@ -58,5 +60,14 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
+    vowel = ['a','e','i','o','u','A','E','I','O','U']
+    tmpstr = input_string[:]
+    ans = ""
+    for i in tmpstr:
+        if i in vowel:
+            continue
+        else:
+            ans += i
+
+    no_vowel_string = ans
     return no_vowel_string
